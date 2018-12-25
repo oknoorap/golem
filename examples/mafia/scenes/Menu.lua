8e93f6eb15e721fa4e9934c78908c520ee18e8b0
oknoorap <oknoorap>
2018-12-25 07:34:42 +0700

    propose new concept

--- examples/mafia/scenes/Menu.lua
+++ examples/mafia/scenes/Menu.lua
@@ -2,61 +2,109 @@ local Object = require('golem.object')
 local Scene = require('golem.scene')
 
 -- New scene with black background
-local MenuScene = Scene:new({
-  background = {
-    color = {
-      r = 0,
-      g = 0,
-      b = 0,
-      a = 255
+local MenuScene = Scene({
+  props = {
+    bg = {
+      color = 'rgba(0, 0, 0, 1)'
     }
-  }
+  },
+
+  state = {
+    image = {
+      x = 0,
+      y = 0
+    },
+    displayRect = false,
+    elapsed = 0
+  },
+
+  init = function(self)
+    self.image =Object.image(game.asset('mafia.jpg'), {
+      size = {
+        width = 200,
+        height = 200
+      }
+    })
+
+    self.rect = Object.rect({
+      size = {
+        width = 200,
+        heigt = 300
+      },
+
+      bg = {
+        color = 'rgba(255, 0, 0, 1)'
+      },
+
+      border = {
+        color = 'rgba(0, 0, 0, 0.2)',
+        width = 2
+      }
+    })
+  end,
+
+  enter = function(self)
+    print('enter scene')
+  end,
+
+  leave = function(self)
+    print('leave scene')
+  end,
+
+  ticker = function(self, elapsed)
+    self.setState({
+      elapsed = elapsed
+    })
+  end,
+
+  input = function(self, key)
+    if key == game.input.key.ENTER then
+      print('scene input')
+    end
+  end,
+
+  _handleImageInput = function(self, key)
+    if key == game.input.key.RIGHT then
+      self.setState({
+        image = {
+          x = self.state.image.x + 1
+        }
+      })
+    end
+  end,
+
+  _handleImageClick = function(self, event)
+    if event.type == game.input.mouse.LEFT then
+      self.setState({
+        displayRect = not self.state.displayRect
+      })
+    end
+  end, 
+
+  render = function(self, draw)
+    if self.state.displayRect == false then
+      draw(self.image, {
+        position = {
+          x = self.state.image.x,
+          y = self.state.image.y
+        },
+  
+        events = {
+          oninput = self._handleImageInput,
+          onclick = self._handleImageClick
+        }
+      })
+    end
+
+    if self.state.displayRect == true then
+      draw(self.rect, {
+        position = {
+          x = self.state.image.x,
+          y = self.state.image.y
+        }
+      })
+    end
+  end
 })
-
--- Image
--- @type Object:image
-local image = Object:image('assets/mafia.jpg', 200, 150)
-
--- Rectangle
--- @type Object:rectangle
-local rect = Object:rectangle(250, 250)
-
--- -- BG Sound
--- local bgsound = Object:sound('bgsound', 'assets/bg.mp3')
-
--- -- Player
--- local player = Object:character('player', {
---   hp: 100
--- })
-
--- -- Enemy
--- local enemy = Object:character('enemy', {
---   x: 100,
---   y: 100,
---   hp: 50
--- })
-
-function MenuScene:canvas()
-  -- Add objects to canvas
-  self:add('background', image)
-  self:add('rect', rect)
-  -- self:add(bgsound)
-  -- self:add(player)
-  -- self:add(enemy)
-end
-
-function MenuScene:enter()
-  print('enter scene')
-end
-
-function MenuScene:action()
-  -- player:input('w', function ()
-  --   player:moveUp()
-  -- end)
-
-  -- player:collisionWith(enemy, function ()
-  --   print('Game over')
-  -- end)
-end
 
 return MenuScene
